01d8502cc22d52f01c8b298aadc27a03ac3cdbd7
wolskikarol <wolskikarol22@gmail.com>
2022-11-11 19:59:46 +0100

    Dodawanie użytkowników do bazy danych

--- app/src/main/java/org/meicode/menadzerwydatkow/registerPage.java
+++ app/src/main/java/org/meicode/menadzerwydatkow/registerPage.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,15 +14,24 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class registerPage extends AppCompatActivity {
+    public static final String TAG = "TAG";
     EditText regMail,regLogin,regPassword,regCoPassword;
     Button regBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar3;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,7 @@ public class registerPage extends AppCompatActivity {
         regBtn          = findViewById(R.id.regBtn);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar3 = findViewById(R.id.progressBar3);
 
         if (fAuth.getCurrentUser() != null) {
@@ -49,7 +60,7 @@ public class registerPage extends AppCompatActivity {
             public void onClick(View view) {
                 String email = regMail.getText().toString().trim();
                 String password = regPassword.getText().toString().trim();
-
+                String login = regLogin.getText().toString();
                 if(TextUtils.isEmpty(email)){
                     regMail.setError("Email id Required");
                     return;
@@ -75,7 +86,18 @@ public class registerPage extends AppCompatActivity {
                    public void onComplete(@NonNull Task<AuthResult> task) {
                        if (task.isSuccessful()) {
                            Toast.makeText(registerPage.this, "User Created.", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),startPage.class));
+                           userID = fAuth.getCurrentUser().getUid();
+                           DocumentReference documentReference = fStore.collection("users").document(userID);
+                           Map<String,Object> user = new HashMap<>();
+                           user.put("login",login);
+                           user.put("email",email);
+                           documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                               @Override
+                               public void onSuccess(Void unused) {
+                                   Log.d(TAG,"onSuccess: user Profile is created for "+ userID);
+                               }
+                           });
+                           startActivity(new Intent(getApplicationContext(),startPage.class));
 
                        }else {
                            Toast.makeText(registerPage.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
