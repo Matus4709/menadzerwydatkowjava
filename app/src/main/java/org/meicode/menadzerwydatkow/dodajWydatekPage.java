3396ad75b5d0a05f315598cafa77cd6d328511dc
wolskikarol <wolskikarol22@gmail.com>
2022-11-12 19:24:43 +0100

    Dodawanie wydatków

--- app/src/main/java/org/meicode/menadzerwydatkow/dodajWydatekPage.java
+++ app/src/main/java/org/meicode/menadzerwydatkow/dodajWydatekPage.java
@@ -1,18 +1,47 @@
 package org.meicode.menadzerwydatkow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class dodajWydatekPage extends AppCompatActivity {
+
+    public static final String TAG = "Wydatek";
+    EditText nazwaWydatku,kategoria,kwota;
+    Button dodajWydatekBtn;
+    FirebaseFirestore fStore;
+    String userID;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wydatek_page);
+
+        nazwaWydatku    = findViewById(R.id.Nazwa_wydatku);
+        kategoria       = findViewById(R.id.kategoria);
+        kwota           = findViewById(R.id.Suma);
+        dodajWydatekBtn = findViewById(R.id.DodajWydatekButton);
+
+
+        fStore = FirebaseFirestore.getInstance();
+        fAuth  = FirebaseAuth.getInstance();
 
         //NAWIGACJA
         final TextView Start = findViewById(R.id.navStart);
@@ -45,6 +74,37 @@ public class dodajWydatekPage extends AppCompatActivity {
             }
         });
         //KONIEC NAWIGACJI
+
+
+        //Dodawanie wydatku
+        dodajWydatekBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nazwaWdk = nazwaWydatku.getText().toString();
+                String category = kategoria.getText().toString();
+                String suma = kwota.getText().toString();
+                userID = fAuth.getCurrentUser().getUid();
+                DocumentReference docReference = fStore.collection("users").document(userID).collection("wydatki").document();
+                Map<String, Object> wydatek = new HashMap<>();
+                wydatek.put("Nazwa",nazwaWdk);
+                wydatek.put("Kategoria", category);
+                wydatek.put("Kwota",suma);
+                docReference.set(wydatek).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG, "Wydatek zapisany!");
+                        startActivity(new Intent(getApplicationContext(),startPage.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Nie udało się zapisać wydatku!");
+                    }
+                });
+            }
+        });
+
+
 
     }
 }
