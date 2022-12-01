c3c6bf68bc8f3d2a29e61c56769b666d0b3231aa
Matus4709 <102665033+Matus4709@users.noreply.github.com>
2022-12-01 10:58:33 +0100

    Utworzenie systemu powiadomień.

--- app/src/main/java/org/meicode/menadzerwydatkow/ustawieniaPage.java
+++ app/src/main/java/org/meicode/menadzerwydatkow/ustawieniaPage.java
@@ -1,28 +1,28 @@
 package org.meicode.menadzerwydatkow;
 
+import static org.meicode.menadzerwydatkow.App.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-
-import com.google.android.material.navigation.NavigationView;
 public class ustawieniaPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
@@ -31,17 +31,33 @@ public class ustawieniaPage extends AppCompatActivity implements NavigationView.
     Button btnlightdark;
     Boolean isDarkModeOn=false;
 
+    double limit = 1;
+
+
+    private NotificationManagerCompat notificationManager;
+    private String editTextTitle;
+    private String editTextMessage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ustawienia_page);
+
+        notificationManager = NotificationManagerCompat.from(this);
+        if (getLimit()==1)
+        {
+            sendOnChannel1();
+        }
+
+
         btnlightdark = findViewById(R.id.btndarklight);
 
         isDarkModeOn = getDarkModeStatus();
         if(isDarkModeOn){
-            btnlightdark.setText("On Light Mode");
+            btnlightdark.setText("Włącz tryb jasny");
         }else {
-            btnlightdark.setText("On Dark Mode");
+            btnlightdark.setText("Włącz tryb ciemny");
         }
 
         btnlightdark.setOnClickListener(new View.OnClickListener() {
@@ -49,11 +65,11 @@ public class ustawieniaPage extends AppCompatActivity implements NavigationView.
             public void onClick(View view) {
                 if(isDarkModeOn){
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    btnlightdark.setText("On Dark Mode");
+                    btnlightdark.setText("Włącz tryb ciemny");
                     isDarkModeOn=false;
                 }else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    btnlightdark.setText("On Light Mode");
+                    btnlightdark.setText("Włącz tryb jasny");
                     isDarkModeOn=true;
                 }
             }
@@ -76,6 +92,36 @@ public class ustawieniaPage extends AppCompatActivity implements NavigationView.
 
         navigationView.setCheckedItem(R.id.nav_ustawienia);
 
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+    public void sendOnChannel1()
+    {
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            Notification notification = new Notification.Builder(this, CHANNEL_1_ID)
+                    .setSmallIcon(R.drawable.ic_baseline_attach_money_24)
+                    .setContentTitle("Menadżer Wydatków")
+                    .setContentText("Przekroczyłeś limit wydatków!")
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                    .build();
+            notificationManager.notify(1,notification);
+        }
+    }
+    public void sendOnChannel2(View v)
+    {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            Notification notification = new Notification.Builder(this, CHANNEL_2_ID)
+                    .setSmallIcon(R.drawable.ic_baseline_attach_money_24)
+                    .setContentTitle("Menadżer Wydatków")
+                    .setContentText("Przekroczyłeś limit wydatków! 2")
+                    .setPriority(Notification.PRIORITY_LOW)
+                    .build();
+            notificationManager.notify(2,notification);
+        }
     }
 
     private boolean getDarkModeStatus(){
